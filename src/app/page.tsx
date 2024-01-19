b02f505519ee5b6e03813e6591f7c36b9dca9007
stakbucks <dhassidu11@gmail.com>
2024-01-19 14:09:31 +0900

    test: 배포 테스트용 커밋2 #13

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
     return (
         <main className="flex min-h-screen flex-col items-center justify-between p-24">
             <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-                변경사항을 테스트합니다.
+                변경사항을 테스트합니다.22
             </div>
         </main>
     )
