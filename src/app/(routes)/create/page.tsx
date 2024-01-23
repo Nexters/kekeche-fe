41d278cc6ac939834733623b768671b778ea0871
stakbucks <dhassidu11@gmail.com>
2024-01-23 21:19:08 +0900

    shadcn 관련 lock 파일 수정

--- src/app/(routes)/create/page.tsx
+++ src/app/(routes)/create/page.tsx
@@ -1,9 +1,13 @@
 import CreateCharacterFunnel from '@/components/create-character/create-character-funnel';
+import { PageContainer } from '@/components/ui';
 
+// 로그인 이전에 캐릭터 생성하는 페이지
 export default function Page() {
     return (
         <>
-            <CreateCharacterFunnel />
+            <PageContainer>
+                <CreateCharacterFunnel />
+            </PageContainer>
         </>
     );
 }
--- src/components/create-character/create-character-funnel.tsx
+++ src/components/create-character/create-character-funnel.tsx
@@ -1,6 +1,6 @@
 'use client';
 import { Carousel, CarouselContent, CarouselItem, CarouselApi } from '@/components/ui-shadcn/carousel';
-import { useEffect, useLayoutEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import Header from './header';
 import Title from './title';
 import CreateCharacterProvider from '@/context/create-character-provider';
@@ -33,8 +33,6 @@ export default function CreateCharacterFunnel() {
         });
     }, [api]);
 
-    console.log(api?.scrollProgress());
-
     useEffect(() => {}, [api?.selectedScrollSnap()]);
 
     useEffect(() => {
@@ -50,18 +48,18 @@ export default function CreateCharacterFunnel() {
             <CreateCharacterProvider>
                 <Carousel setApi={setApi} opts={{ watchDrag: false, duration: 10 }}>
                     <CarouselContent>
-                        <CarouselItem className="w-full flex flex-col items-center ">
+                        <CarouselItem className="flex w-full flex-col items-center ">
                             <Header onGoBack={() => router.push('/')} />
                             <Title text="캐릭터의 모습을 고르세요" />
                             <SelectShape />
                             <button onClick={handleNextClick}>다음</button>
                         </CarouselItem>
-                        <CarouselItem className="w-full flex flex-col items-center">
+                        <CarouselItem className="flex w-full flex-col items-center">
                             <Header onGoBack={handlePrevClick} />
                             <Title text="캐릭터의 컬러를 고르세요" />
                             <div>1</div> <button onClick={handleNextClick}>다음</button>
                         </CarouselItem>
-                        <CarouselItem className="w-full flex flex-col items-center">
+                        <CarouselItem className="flex w-full flex-col items-center">
                             <Header onGoBack={handlePrevClick} />
                             <Title text="캐릭터의 성격을 고르세요" />
                             <div>2</div> <button onClick={handleNextClick}>다음</button>
