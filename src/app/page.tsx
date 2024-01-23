f286fa0781bd4f7fe9a503c298759f6b77822f68
stakbucks <dhassidu11@gmail.com>
2024-01-23 21:30:16 +0900

    파일명 index.tsx로 수정

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,17 @@
 import { PageContainer } from '@/components/ui';
+import Link from 'next/link';
 
 export default function Home() {
     return (
         <PageContainer hasNavigator>
             <div className="grid flex-1 place-items-center">
                 <h1>캐릭캐릭 다이어리🧚‍♀️</h1>
+                {/**
+                 * pwa용 임시 버튼
+                 */}
+                <Link href="/create">
+                    <button>캐릭터 생성하기</button>
+                </Link>
             </div>
         </PageContainer>
     );
