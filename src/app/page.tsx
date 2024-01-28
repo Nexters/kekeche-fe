c95992560bcdf5a422c24d1ebb932ab80335bece
황 태환 <asst2401095@tosspartners.com>
2024-01-28 18:27:52 +0900

    원래 코드로 돌려 놓기

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,9 @@
-'use client';
-
 import CtaButton from '@/components/create-character/cta-button';
 import FixedBottomArea from '@/components/create-character/fixed-bottom-area';
 import Intro from '@/components/create-character/intro';
 import { PageContainer } from '@/components/ui';
-import { useToast } from '@/components/ui-shadcn/toast/use-toast';
 import KakaoLoginButton from '@/components/ui/KakaoLoginButton';
 import Modal from '@/components/ui/modal';
-import { DialogClose } from '@radix-ui/react-dialog';
 import Link from 'next/link';
 
 export default function Home() {
@@ -17,12 +13,6 @@ export default function Home() {
             {/**
              * TODO: 로고 추가
              */}
-            <Modal
-                triggerElement={<button>모달 ㄱ</button>}
-                title="모달 테스트중.."
-                description="모달이 잘 작동할까요"
-                contents={<DialogClose>꺼</DialogClose>}
-            />
             <FixedBottomArea className="mb-[31px] gap-[16px]">
                 <KakaoLoginButton />
                 <Link href="/create">
