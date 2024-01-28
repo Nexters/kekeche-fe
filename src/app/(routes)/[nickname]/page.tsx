183ec961ea89d52c6ae8e277dd4df8b092da9325
Jay Lee <roof2586@naver.com>
2024-01-28 15:56:39 +0900

    feat: 캐릭터 목록 퍼블리싱 #5

--- src/app/(routes)/[nickname]/page.tsx
+++ src/app/(routes)/[nickname]/page.tsx
@@ -1,13 +1,14 @@
-import MockCharacterImage from '@/assets/images/mock_chracter_80x80.png';
+import BeanIcon from '@/assets/icons/bean_pink.svg';
+import MockCharacterImage from '@/assets/images/mock_character_120x120.png';
 import { PageContainer } from '@/components/ui';
 import Image from 'next/image';
 import Link from 'next/link';
 
-export default function Home(props: { params: { nickname: string } }) {
+export default function Home() {
     return (
         <PageContainer hasNavigator>
-            <div className="mb-2 px-6 py-2 text-bold24">{props.params.nickname}님의 캐릭터</div>
-            <div className="grid grid-cols-2 gap-3 px-6 py-2">
+            <div className="mb-2 py-5 text-center text-[24px] font-bold leading-8">준근의 도감</div>
+            <div className="grid grid-cols-2 gap-3 px-6 py-4">
                 {Array(4)
                     .fill(0)
                     .map((_, i) => {
@@ -15,27 +16,26 @@ export default function Home(props: { params: { nickname: string } }) {
                             <Link
                                 href=""
                                 key={i}
-                                className="flex h-[200px] w-40 flex-col items-center justify-center gap-1 rounded-xl bg-backgroundSecondaryLight px-4"
+                                className="flex flex-col items-center justify-center rounded-2xl bg-[#FFF3F4] px-4 py-[18px] "
                             >
-                                <div className="flex flex-col items-center text-semibold16 text-contentPrimaryLight">
-                                    <div>
-                                        <Image alt="몰랑이" src={MockCharacterImage} />
-                                    </div>
-                                    <div className="flex flex-col text-semibold16">
-                                        <span>책 읽어야대</span>
-                                        <span>Lv.12</span>
-                                    </div>
+                                <div className="mb-2 flex items-center rounded-full bg-[#FFC9D0] px-2 py-[6px]">
+                                    <span className="h-4 w-4">
+                                        <BeanIcon />
+                                    </span>
+                                    <span className="text-[12px] font-semibold leading-3 text-[#E57897]">Lv.5</span>
                                 </div>
-                                <div className="flex flex-wrap gap-[2.6px]">
-                                    <div className="grid h-[22px] place-items-center rounded-sm bg-gray-400 px-2">
-                                        <span className="min-w-fit text-[10px] text-gray-100">#열정적</span>
-                                    </div>
-                                    <div className="grid h-[22px] place-items-center rounded-sm  bg-gray-400 px-2">
-                                        <span className="text-[10px] text-gray-100">#꼼꼼한</span>
-                                    </div>
-
-                                    <div className="grid h-[22px] place-items-center rounded-sm  bg-gray-400 px-2">
-                                        <span className="min-w-fit text-[10px] text-gray-100">#열정적</span>
+                                <div className="mb-1">
+                                    <Image alt="몰랑이" src={MockCharacterImage} />
+                                </div>
+                                <p className="mb-1 text-semibold14 text-contentPrimaryLight">넥터 PM</p>
+                                <div className="h-[15px] w-full">
+                                    <div className="flex items-center gap-1">
+                                        <span className="relative h-2 flex-1 rounded-full bg-gray-200">
+                                            <span className="inest-0 absolute h-full w-1/3 rounded-full bg-[#E57897]" />
+                                        </span>
+                                        <span className="text-[10px] font-semibold leading-[15px] text-[#565E71]">
+                                            00/10
+                                        </span>
                                     </div>
                                 </div>
                             </Link>
@@ -49,7 +49,7 @@ export default function Home(props: { params: { nickname: string } }) {
 
 function CharacterCreateButton() {
     return (
-        <Link href="" className="grid h-[200px] w-40 place-items-center rounded-xl bg-backgroundSecondaryLight">
+        <Link href="" className="grid h-[236px]  place-items-center rounded-xl bg-backgroundSecondaryLight">
             <svg width="57" height="57" viewBox="0 0 57 57" fill="none" xmlns="http://www.w3.org/2000/svg">
                 <path
                     d="M56.75 32.1797H32.75V56.1797H24.75V32.1797H0.75V24.1797H24.75V0.179688H32.75V24.1797H56.75V32.1797Z"
