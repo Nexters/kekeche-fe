8a99a4bd7d6e57e608e4b23d0c591fad03f862e2
Jay Lee <86228307+Jxxunnn@users.noreply.github.com>
2024-01-28 15:07:36 +0900

    Feature/gnb (#19)
    * svg파일에 current 속성 추가 #3
    
    * url 구조 변경 #3
    
    * 네비게이션 컴포넌트 이름 변경 #3
    
    * 네비게이션 컴포넌트 리팩토링 #3

--- src/components/ui/page-container/index.tsx
+++ src/components/ui/page-container/index.tsx
@@ -1,24 +1,20 @@
 import { ReactNode } from 'react';
-import TabNavigator from './tab-navigator';
+import BottomTabNavigator from './bottom-tab-navigator';
 
 type Props = {
     children: ReactNode;
-    hasNavigator?: boolean;
+    hasNavigator: boolean;
 };
 
 export default function PageContainer({ children, hasNavigator }: Props) {
     return (
         <div className="bg-[#f5f5f5]">
             <div
-                className="bg-white mx-auto flex min-h-screen w-full flex-col shadow-lg md:w-[375px] "
+                className="mx-auto flex min-h-screen w-full flex-col bg-white shadow-lg md:w-[375px] "
                 style={{ minHeight: '100dvh' }}
             >
                 <div className="flex flex-1 flex-col overflow-auto">{children}</div>
-                {hasNavigator && (
-                    <div className="border-top bg-white sticky bottom-0 left-0 right-0 z-10 border-t border-t-[#E8EAEE]">
-                        <TabNavigator />
-                    </div>
-                )}
+                {hasNavigator && <BottomTabNavigator />}
             </div>
         </div>
     );
