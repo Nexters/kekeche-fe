8765b43ce4cc3e7305090644c8884f01fd8a2687
Jay Lee <roof2586@naver.com>
2024-01-29 22:46:08 +0900

    feat: 기록작성 퍼블리싱

--- src/app/(routes)/memos/create/page.tsx
+++ src/app/(routes)/memos/create/page.tsx
@@ -1,61 +1,106 @@
-import MockCharacterImage from '@/assets/images/mock_chracter_80x80.png';
+'use client';
+
+import BackArrowIcon from '@/assets/icons/arrow-left_24x24.svg';
 import { PageContainer } from '@/components/ui';
-import Image from 'next/image';
-import Link from 'next/link';
+import React from 'react';
+import ContentEditable, { ContentEditableEvent } from 'react-contenteditable';
 
 export default function Home() {
     return (
-        <PageContainer hasNavigator>
-            <div className="mb-2 px-6 py-2 text-bold24">윤서님의 캐릭터</div>
-            <div className="grid grid-cols-2 gap-3 px-6 py-2">
-                {Array(4)
-                    .fill(0)
-                    .map((_, i) => {
-                        return (
-                            <Link
-                                href=""
-                                key={i}
-                                className="flex h-[200px] w-40 flex-col items-center justify-center gap-1 rounded-xl bg-backgroundSecondaryLight px-4"
-                            >
-                                <div className="flex flex-col items-center text-semibold16 text-contentPrimaryLight">
-                                    <div>
-                                        <Image alt="몰랑이" src={MockCharacterImage} />
-                                    </div>
-                                    <div className="flex flex-col text-semibold16">
-                                        <span>책 읽어야대</span>
-                                        <span>Lv.12</span>
-                                    </div>
-                                </div>
-                                <div className="flex flex-wrap gap-[2.6px]">
-                                    <div className="grid h-[22px] place-items-center rounded-sm bg-gray-400 px-2">
-                                        <span className="min-w-fit text-[10px] text-gray-100">#열정적</span>
-                                    </div>
-                                    <div className="grid h-[22px] place-items-center rounded-sm  bg-gray-400 px-2">
-                                        <span className="text-[10px] text-gray-100">#꼼꼼한</span>
-                                    </div>
-
-                                    <div className="grid h-[22px] place-items-center rounded-sm  bg-gray-400 px-2">
-                                        <span className="min-w-fit text-[10px] text-gray-100">#열정적</span>
-                                    </div>
-                                </div>
-                            </Link>
-                        );
-                    })}
-                <CharacterCreateButton />
-            </div>
+        <PageContainer>
+            <header className="mb-[10px] flex justify-between gap-2">
+                <button aria-label="뒤로 가기 버튼" className="p-3">
+                    <BackArrowIcon stroke="#8E939E" />
+                </button>
+                <span className=" grid flex-1 place-items-center text-center text-[18px] font-semibold leading-7 text-gray-500">
+                    기록 작성
+                </span>
+                <button className="p-3 text-semibold16 text-[#1E73F3]">저장</button>
+            </header>
+            <section>
+                <Editor />
+            </section>
         </PageContainer>
     );
 }
 
-function CharacterCreateButton() {
-    return (
-        <Link href="" className="grid h-[200px] w-40 place-items-center rounded-xl bg-backgroundSecondaryLight">
-            <svg width="57" height="57" viewBox="0 0 57 57" fill="none" xmlns="http://www.w3.org/2000/svg">
-                <path
-                    d="M56.75 32.1797H32.75V56.1797H24.75V32.1797H0.75V24.1797H24.75V0.179688H32.75V24.1797H56.75V32.1797Z"
-                    fill="#DCDCDC"
+interface EditorState {
+    html: string;
+    textNodeCount: number;
+}
+
+class Editor extends React.Component<{}, EditorState> {
+    constructor(props: {}) {
+        super(props);
+        this.state = { html: '', textNodeCount: 0 };
+    }
+
+    private countTextNodes(html: string): number {
+        const parser = new DOMParser();
+        const doc = parser.parseFromString(html, 'text/html');
+        const textNodes = doc.body.childNodes;
+
+        let count = 0;
+
+        const countTextContent = (node: Node) => {
+            if (node.nodeType === 3) {
+                // TEXT_NODE
+                const textContent = (node as Text).textContent || '';
+                count += textContent.length;
+            } else {
+                for (let i = 0; i < node.childNodes.length; i++) {
+                    countTextContent(node.childNodes[i]);
+                }
+            }
+        };
+
+        textNodes.forEach((node) => countTextContent(node));
+
+        return count;
+    }
+
+    private handleChange = (event: ContentEditableEvent) => {
+        const html = event.target.value;
+        const textNodeCount = this.countTextNodes(html);
+
+        this.setState({ html, textNodeCount });
+    };
+
+    render = () => {
+        const { html, textNodeCount } = this.state;
+        const regex = /#(\S+)(?=\s|\p{P}|$)/gu; // \S는 공백이 아닌 문자, \p{P}는 구두점을 나타냅니다.
+        const matches = [...html.matchAll(regex)];
+        const hashtags = matches.map((match) => match[1]).map((text) => text.replace('&nbsp;', ''));
+
+        const updatedContent = html.replace(/#(\S+)(?=\s|$)/g, (_, keyword) => {
+            return ` <span class="keyword">#${keyword}</span>`;
+        });
+
+        // <div> 태그를 <br> 태그로 변환하여 줄 바꿈이 발생하도록 함
+        let formattedContent = updatedContent.replace(/<div>/g, '<br>');
+
+        if (formattedContent.length > 200) {
+            formattedContent = formattedContent.substring(0, 200);
+        }
+
+        return (
+            <div className="px-6 py-[10px] text-gray-500">
+                <ContentEditable
+                    className="mb-[10px] break-all focus:outline-none"
+                    html={formattedContent}
+                    // @ts-ignore
+                    placeholder="메모를 입력해주세요"
+                    disabled={false}
+                    onChange={this.handleChange}
                 />
-            </svg>
-        </Link>
-    );
+                <style>{`
+           
+                    .keyword {
+                    text-decoration: underline;
+                    }
+                    `}</style>
+                <p className="py-2 text-right text-gray-300">{textNodeCount}/200</p>
+            </div>
+        );
+    };
 }
