a6fec95a5035a9ec5099952ab1bf8f44d8e851a0
황 태환 <asst2401095@tosspartners.com>
2024-01-30 20:31:42 +0900

    feat: memos 페이지 헤더, 검색 인풋 생성

---
+++ src/app/(routes)/memos/_components/Header.tsx
@@ -0,0 +1,7 @@
+type Props = {
+    text: string;
+};
+
+export function Header({ text }: Props) {
+    return <div className="w-full px-[24px] py-[8px] text-[24px] font-[700] leading-[32px] text-[#2E2E36]">{text}</div>;
+}
---
+++ src/app/(routes)/memos/_components/SearchBox.tsx
@@ -0,0 +1,16 @@
+import SearchIcon from '@/assets/icons/search_20x20.svg';
+
+export default function SearchBox() {
+    return (
+        <div className="w-full px-[24px] py-[16px]">
+            <div className="flex h-[48px] w-[328px] gap-[10px] rounded-[30px] border-[1.4px] border-solid border-[#-[#DFE2EA]] bg-[#FCFDFF] px-[16px] py-[12px]">
+                <input
+                    type="text"
+                    className="text-black h-full w-[266px] border-none text-regular16 outline-none"
+                    placeholder="해시태그를 입력하세요"
+                />
+                <SearchIcon />
+            </div>
+        </div>
+    );
+}
