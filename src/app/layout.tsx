a202ef513026a590bfa5d630862fa6c88bb92aa7
Jay Lee <roof2586@naver.com>
2024-02-01 23:42:22 +0900

    스플래시 이미지

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -22,6 +22,41 @@ export const metadata: Metadata = {
     icons: [
         { rel: 'apple-touch-icon', url: '/icons/icon-128x128.png' },
         { rel: 'icon', url: '/icons/icon-128x128.png' },
+        {
+            url: '/splashscreens/iphone5_splash.png',
+            media: '(device-width: 320px) and (device-height: 568px) and (-webkit-device-pixel-ratio: 2)',
+            rel: 'apple-touch-startup-image',
+        },
+        {
+            url: '/splashscreens/iphone6_splash.png',
+            media: '(device-width: 375px) and (device-height: 667px) and (-webkit-device-pixel-ratio: 2)',
+            rel: 'apple-touch-startup-image',
+        },
+        {
+            url: '/splashscreens/iphoneplus_splash.png',
+            media: '(device-width: 621px) and (device-height: 1104px) and (-webkit-device-pixel-ratio: 3)',
+            rel: 'apple-touch-startup-image',
+        },
+        {
+            url: '/splashscreens/iphonex_splash.png',
+            media: '(device-width: 621px) and (device-height: 1104px) and (-webkit-device-pixel-ratio: 3)',
+            rel: 'apple-touch-startup-image',
+        },
+        {
+            url: '/splashscreens/iphonexr_splash.png',
+            media: '(device-width: 375px) and (device-height: 812px) and (-webkit-device-pixel-ratio: 3)',
+            rel: 'apple-touch-startup-image',
+        },
+        {
+            url: '/splashscreens/iphonexr_splash.png',
+            media: '(device-width: 414px) and (device-height: 896px) and (-webkit-device-pixel-ratio: 2)',
+            rel: 'apple-touch-startup-image',
+        },
+        {
+            url: '/splashscreens/iphonexr_splash.png',
+            media: '(device-width: 414px) and (device-height: 896px) and (-webkit-device-pixel-ratio: 2)',
+            rel: 'apple-touch-startup-image',
+        },
     ],
 };
 
@@ -62,21 +97,6 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
                 <link
                     href="/splashscreens/ipad_splash.png"
                     media="(device-width: 768px) and (device-height: 1024px) and (-webkit-device-pixel-ratio: 2)"
-                    rel="apple-touch-startup-image"
-                />
-                <link
-                    href="/splashscreens/ipadpro1_splash.png"
-                    media="(device-width: 834px) and (device-height: 1112px) and (-webkit-device-pixel-ratio: 2)"
-                    rel="apple-touch-startup-image"
-                />
-                <link
-                    href="/splashscreens/ipadpro3_splash.png"
-                    media="(device-width: 834px) and (device-height: 1194px) and (-webkit-device-pixel-ratio: 2)"
-                    rel="apple-touch-startup-image"
-                />
-                <link
-                    href="/splashscreens/ipadpro2_splash.png"
-                    media="(device-width: 1024px) and (device-height: 1366px) and (-webkit-device-pixel-ratio: 2)"
                     rel="apple-touch-startup-image"
                 />
                 <meta name="naver-site-verification" content="6f87b8170cef8604157538c5f66fffce3e688bc2" />
