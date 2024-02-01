339eee871ac8b8266ce616e478c57996c5e01da5
Jay Lee <roof2586@naver.com>
2024-02-01 23:32:53 +0900

    web icons

--- next.config.js
+++ next.config.js
@@ -9,12 +9,12 @@ const withPWA = require('@ducanh2912/next-pwa').default({
     workboxOptions: {
         disableDevLogs: true,
     },
-})
+});
 
 const nextConfig = {
     webpack(config) {
         // Grab the existing rule that handles SVG imports
-        const fileLoaderRule = config.module.rules.find((rule) => rule.test?.test?.('.svg'))
+        const fileLoaderRule = config.module.rules.find((rule) => rule.test?.test?.('.svg'));
 
         config.module.rules.push(
             // Reapply the existing rule, but only for svg imports ending in ?url
@@ -30,13 +30,13 @@ const nextConfig = {
                 resourceQuery: { not: [...fileLoaderRule.resourceQuery.not, /url/] }, // exclude if *.svg?url
                 use: ['@svgr/webpack'],
             },
-        )
+        );
 
         // Modify the file loader rule to ignore *.svg, since we have it handled now.
-        fileLoaderRule.exclude = /\.svg$/i
+        fileLoaderRule.exclude = /\.svg$/i;
 
-        return config
+        return config;
     },
-}
+};
 
-module.exports = withPWA(nextConfig)
+module.exports = withPWA(nextConfig);
