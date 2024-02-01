c8f394c131a1d8e678740dac24c25ce0306966da
Jay Lee <roof2586@naver.com>
2024-02-01 23:17:35 +0900

    스플래시스크린

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,8 +15,8 @@ export const viewport: Viewport = {
 };
 
 export const metadata: Metadata = {
-    applicationName: '캐릭캐릭 다이어리',
-    title: '캐릭캐릭 다이어리',
+    applicationName: '어나더 미',
+    title: '어나더 미',
     description: '나만의 작은 세계를 펼치는 특별한 일기',
     manifest: '/manifest.json',
     icons: [
@@ -30,52 +30,52 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
         <html lang="ko">
             <head>
                 <link
-                    href="splashscreens/iphone5_splash.png"
+                    href="/splashscreens/iphone5_splash.png"
                     media="(device-width: 320px) and (device-height: 568px) and (-webkit-device-pixel-ratio: 2)"
                     rel="apple-touch-startup-image"
                 />
                 <link
-                    href="splashscreens/iphone6_splash.png"
+                    href="/splashscreens/iphone6_splash.png"
                     media="(device-width: 375px) and (device-height: 667px) and (-webkit-device-pixel-ratio: 2)"
                     rel="apple-touch-startup-image"
                 />
                 <link
-                    href="splashscreens/iphoneplus_splash.png"
+                    href="/splashscreens/iphoneplus_splash.png"
                     media="(device-width: 621px) and (device-height: 1104px) and (-webkit-device-pixel-ratio: 3)"
                     rel="apple-touch-startup-image"
                 />
                 <link
-                    href="splashscreens/iphonex_splash.png"
+                    href="/splashscreens/iphonex_splash.png"
                     media="(device-width: 375px) and (device-height: 812px) and (-webkit-device-pixel-ratio: 3)"
                     rel="apple-touch-startup-image"
                 />
                 <link
-                    href="splashscreens/iphonexr_splash.png"
+                    href="/splashscreens/iphonexr_splash.png"
                     media="(device-width: 414px) and (device-height: 896px) and (-webkit-device-pixel-ratio: 2)"
                     rel="apple-touch-startup-image"
                 />
                 <link
-                    href="splashscreens/iphonexsmax_splash.png"
+                    href="/splashscreens/iphonexsmax_splash.png"
                     media="(device-width: 414px) and (device-height: 896px) and (-webkit-device-pixel-ratio: 3)"
                     rel="apple-touch-startup-image"
                 />
                 <link
-                    href="splashscreens/ipad_splash.png"
+                    href="/splashscreens/ipad_splash.png"
                     media="(device-width: 768px) and (device-height: 1024px) and (-webkit-device-pixel-ratio: 2)"
                     rel="apple-touch-startup-image"
                 />
                 <link
-                    href="splashscreens/ipadpro1_splash.png"
+                    href="/splashscreens/ipadpro1_splash.png"
                     media="(device-width: 834px) and (device-height: 1112px) and (-webkit-device-pixel-ratio: 2)"
                     rel="apple-touch-startup-image"
                 />
                 <link
-                    href="splashscreens/ipadpro3_splash.png"
+                    href="/splashscreens/ipadpro3_splash.png"
                     media="(device-width: 834px) and (device-height: 1194px) and (-webkit-device-pixel-ratio: 2)"
                     rel="apple-touch-startup-image"
                 />
                 <link
-                    href="splashscreens/ipadpro2_splash.png"
+                    href="/splashscreens/ipadpro2_splash.png"
                     media="(device-width: 1024px) and (device-height: 1366px) and (-webkit-device-pixel-ratio: 2)"
                     rel="apple-touch-startup-image"
                 />
