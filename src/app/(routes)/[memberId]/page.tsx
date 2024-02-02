27ad786d3c61bdb2b736a7b91e8a2459f55cd7a3
Jay Lee <roof2586@naver.com>
2024-02-03 04:54:02 +0900

    홈타이틀

--- src/app/(routes)/[memberId]/page.tsx
+++ src/app/(routes)/[memberId]/page.tsx
@@ -27,7 +27,9 @@ export default function Home({ params: { memberId } }: { params: { memberId: str
 
     return (
         <PageContainer hasNavigator={charactersResponse?.isMe}>
-            <div className="mb-2 py-5 text-center text-[24px] font-bold leading-8">나의 도감</div>
+            <div className="mb-2 py-5 text-center text-[24px] font-bold leading-8">
+                {!charactersResponse ? '' : charactersResponse?.isMe ? '나' : '누군가'}의 도감
+            </div>
             <div className="grid grid-cols-2 gap-3 px-6 py-4">
                 {charactersResponse?.characters?.map((character, i) => {
                     return (
