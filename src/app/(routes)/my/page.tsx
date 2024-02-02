7a7f920ae24e8c192ab5ddfd11fcde0c58f78c30
Jay Lee <roof2586@naver.com>
2024-02-02 23:45:43 +0900

    마이페이지 개발

--- src/app/(routes)/my/page.tsx
+++ src/app/(routes)/my/page.tsx
@@ -1,61 +1,100 @@
-import MockCharacterImage from '@/assets/images/mock_chracter_80x80.png';
-import { PageContainer } from '@/components/ui';
-import Image from 'next/image';
-import Link from 'next/link';
+'use client';
 
-export default function Home() {
+import ChevronRightIcon from '@/assets/icons/chevron-right_20x20.svg';
+import { PageContainer } from '@/components/ui';
+import { useToast } from '@/components/ui-shadcn/toast/use-toast';
+import getMember, { GetMemberResponse } from '@/services/getMember';
+import { deleteCookie, getCookie } from 'cookies-next';
+import { usePathname, useRouter } from 'next/navigation';
+import { useEffect, useState } from 'react';
+
+export default function My() {
+    const [memberResponse, setMemberResponse] = useState<GetMemberResponse | undefined>(undefined);
+    const router = useRouter();
+
+    const pathname = usePathname();
+
+    useEffect(() => {
+        getMember({ accessToken: getCookie('accessToken') }).then((res) => setMemberResponse(res));
+    }, []);
+
+    const { toast } = useToast();
+
     return (
         <PageContainer hasNavigator>
-            <div className="mb-2 px-6 py-2 text-bold24">윤서님의 캐릭터</div>
-            <div className="grid grid-cols-2 gap-3 px-6 py-2">
-                {Array(4)
-                    .fill(0)
-                    .map((_, i) => {
-                        return (
-                            <Link
-                                href=""
-                                key={i}
-                                className="flex h-[200px] w-40 flex-col items-center justify-center gap-1 rounded-xl bg-backgroundSecondaryLight px-4"
-                            >
-                                <div className="flex flex-col items-center text-semibold16 text-contentPrimaryLight">
-                                    <div>
-                                        <Image alt="몰랑이" src={MockCharacterImage} />
-                                    </div>
-                                    <div className="flex flex-col text-semibold16">
-                                        <span>책 읽어야대</span>
-                                        <span>Lv.12</span>
-                                    </div>
-                                </div>
-                                <div className="flex flex-wrap gap-[2.6px]">
-                                    <div className="grid h-[22px] place-items-center rounded-sm bg-gray-400 px-2">
-                                        <span className="min-w-fit text-[10px] text-gray-100">#열정적</span>
-                                    </div>
-                                    <div className="grid h-[22px] place-items-center rounded-sm  bg-gray-400 px-2">
-                                        <span className="text-[10px] text-gray-100">#꼼꼼한</span>
-                                    </div>
+            <div className="mb-2 px-6 py-2 text-bold24">내 정보</div>
+            <section>
+                <div className="flex h-[100px] justify-between gap-3 px-6 py-3">
+                    <div className="grid h-full min-w-[160px] flex-1  place-items-center rounded-xl bg-[#F7F8F9] px-6 py-4">
+                        <span className="text-regular14 text-[#3D4350]">총 캐릭터</span>
+                        <span className="text-semibold16 text-[#17171B]">{memberResponse?.characterCount ?? 0}개</span>
+                    </div>
+                    <div className="grid h-full min-w-[160px] flex-1  place-items-center rounded-xl bg-[#F7F8F9] px-6 py-4">
+                        <span className="text-regular14 text-[#3D4350]">총 기록</span>
+                        <span className="text-semibold16 text-[#17171B]">{memberResponse?.memoCount ?? 0}개</span>
+                    </div>
+                </div>
+            </section>
+            <div className="h-3 bg-[#F7F8F9]" role="presentation" />
+            <ul>
+                <li>
+                    <a
+                        target="_blank"
+                        href="https://forms.gle/S5ARuAhkL8tr9QeU8"
+                        className="flex w-full items-center justify-between p-6"
+                    >
+                        <span className="text-regular16 text-[#4B4F58]">의견 보내기</span>
+                        <ChevronRightIcon />
+                    </a>
+                </li>
+                <li>
+                    <button
+                        onClick={async () => {
+                            const shareData = {
+                                title: '다양한 나를 키우는 AnotherMe',
+                                text: '나의 캐릭터별 성장기록 서비스',
+                                url: `${location.origin}/${memberResponse?.memberId}`,
+                            };
 
-                                    <div className="grid h-[22px] place-items-center rounded-sm  bg-gray-400 px-2">
-                                        <span className="min-w-fit text-[10px] text-gray-100">#열정적</span>
-                                    </div>
-                                </div>
-                            </Link>
-                        );
-                    })}
-                <CharacterCreateButton />
-            </div>
+                            if (navigator.canShare && navigator.canShare(shareData)) {
+                                navigator.share(shareData);
+                            } else {
+                                toast({
+                                    description: 'URL이 클립보드에 복사되었어요.',
+                                });
+                            }
+                        }}
+                        className="flex w-full items-center justify-between p-6"
+                    >
+                        <span className="text-regular16 text-[#4B4F58]">친구 초대하기</span>
+                        <ChevronRightIcon />
+                    </button>
+                </li>
+                <li>
+                    <button className="flex w-full items-center justify-between p-6">
+                        <span className="text-regular16 text-[#4B4F58]">만든 사람</span>
+                        <ChevronRightIcon />
+                    </button>
+                </li>
+                <li>
+                    <button
+                        onClick={() => {
+                            deleteCookie('accessToken');
+                            router.push('/');
+                        }}
+                        className="flex w-full items-center justify-between p-6"
+                    >
+                        <span className="text-regular16 text-[#4B4F58]">로그아웃</span>
+                        <ChevronRightIcon />
+                    </button>
+                </li>
+                <li>
+                    <button className="flex w-full items-center justify-between p-6">
+                        <span className="text-regular16 text-[#4B4F58]">회원탈퇴</span>
+                        <ChevronRightIcon />
+                    </button>
+                </li>
+            </ul>
         </PageContainer>
-    );
-}
-
-function CharacterCreateButton() {
-    return (
-        <Link href="" className="grid h-[200px] w-40 place-items-center rounded-xl bg-backgroundSecondaryLight">
-            <svg width="57" height="57" viewBox="0 0 57 57" fill="none" xmlns="http://www.w3.org/2000/svg">
-                <path
-                    d="M56.75 32.1797H32.75V56.1797H24.75V32.1797H0.75V24.1797H24.75V0.179688H32.75V24.1797H56.75V32.1797Z"
-                    fill="#DCDCDC"
-                />
-            </svg>
-        </Link>
     );
 }
