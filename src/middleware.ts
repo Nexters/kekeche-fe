41516ef8a39a584c4f9e8d90438410d92342928d
황 태환 <asst2401095@tosspartners.com>
2024-02-06 21:15:35 +0900

    feat: 미들웨어에서 로그인 안한 유저가 로그인 필요한 경로 진입시 홈으로 리다이렉트

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,10 +1,9 @@
 import type { NextRequest } from 'next/server';
 import { NextResponse } from 'next/server';
-import getMember from './services/auth/getMember';
 import { ResponseBody } from './types/response-body';
 
 const HOME_PATH = '/';
-const AUTH_PATHS = ['/characters', '/memos', '/my'];
+const AUTH_PATHS = ['/character', '/memos', '/my']; // 권한이 필요한 페이지들
 
 interface MemberInfo {
     memberId: number;
@@ -15,12 +14,16 @@ interface MemberInfo {
 export async function middleware(request: NextRequest) {
     const accessToken = request.cookies.get('accessToken')?.value;
 
-    if (request.nextUrl.pathname === HOME_PATH) {
-        const response = await fetch(`${process.env.NEXT_PUBLIC_SERVER_BASE_URL}/api/v1/member`, {
+    const getMember = async () =>
+        await fetch(`${process.env.NEXT_PUBLIC_SERVER_BASE_URL}/api/v1/member`, {
             headers: {
                 Authorization: `${accessToken}`,
             },
         });
+
+    // 홈 진입
+    if (request.nextUrl.pathname === HOME_PATH) {
+        const response = await getMember();
         if (response.status === 401) return NextResponse.next(); // 유효하지 않은 토큰인 경우
 
         const {
@@ -30,16 +33,17 @@ export async function middleware(request: NextRequest) {
         return NextResponse.redirect(new URL(`/${memberId}`, request.url));
     }
 
+    // 유저 인증이 필요한 페이지들 진입
     for (const path of AUTH_PATHS) {
         if (request.nextUrl.pathname.startsWith(path)) {
-            const member = await getMember({ accessToken });
-            if (!member) NextResponse.redirect(new URL('/', request.url));
+            try {
+                await getMember().then((res) => res.json());
+                return NextResponse.next();
+            } catch (error) {
+                return NextResponse.redirect(new URL('/', request.url));
+            }
         }
     }
 
     return NextResponse.next();
 }
-
-export const config = {
-    matcher: '/:path',
-};
