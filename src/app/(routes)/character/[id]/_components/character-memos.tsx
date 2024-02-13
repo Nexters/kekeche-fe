bba6fe4c2813ca6cbb193e497aa335cc754f36aa
황 태환 <asst2401095@tosspartners.com>
2024-02-14 01:39:32 +0900

    feat: 메모 없을 때 fallback ui 모듈화

--- src/app/(routes)/character/[id]/_components/character-memos.tsx
+++ src/app/(routes)/character/[id]/_components/character-memos.tsx
@@ -8,6 +8,7 @@ import { getCookie } from 'cookies-next';
 import Image from 'next/image';
 import { usePathname } from 'next/navigation';
 import NoMemoBadgeImg from '@/assets/images/no-memo-character.png';
+import NoMemoFallback from '@/components/no-memo-fallback';
 
 export default function CharacterMemos() {
     const pathname = usePathname();
@@ -27,12 +28,7 @@ export default function CharacterMemos() {
     return (
         <div className="mt-[24px] flex h-auto w-full flex-col items-center rounded-t-[24px] bg-newGray-100 px-[23.5px] pb-[160px] pt-[24px]">
             <h3 className="mb-[20px] w-full text-left text-Subtitle1 text-newGray-900">{`메모 ${totalPages > 0 ? totalPages : ''}`}</h3>
-            {memos.length === 0 && (
-                <div className="mt-[12px] flex w-full flex-col items-center gap-[10px] py-[20px]">
-                    <Image alt="대체 이미지" src={NoMemoBadgeImg} width={80} height={80} />
-                    <p className="text-[18px] font-[500] text-newGray-500">{'아직 작성한 기록이 없어요'}</p>
-                </div>
-            )}
+            {memos.length === 0 && <NoMemoFallback />}
             {memos.length > 0 && (
                 <Memos>
                     {memos.map((memo) => (
--- src/app/(routes)/memos/_components/no-memo.tsx
+++ src/app/(routes)/memos/_components/no-memo.tsx
@@ -1,22 +1,19 @@
 'use client';
 
-import AlertCircle from '@/assets/icons/alert-circle.svg';
+import NoMemoFallback from '@/components/no-memo-fallback';
+import Link from 'next/link';
 import { useRouter } from 'next/navigation';
 
 export default function NoMemo() {
     const router = useRouter();
     return (
         <div className=" mt-[155px] flex w-full flex-col items-center">
-            <AlertCircle />
-            <span className="mt-[10px] text-bold18 text-gray-300">아직 작성한 기록이 없어요</span>
-            <button
-                onClick={() => {
-                    router.push('/memos/create');
-                }}
-                className="bg-purple-200 mt-[25px] h-[48px] w-[118px] rounded-[12px] text-semibold16 text-[#ffffff]"
-            >
-                기록하러 가기
-            </button>
+            <NoMemoFallback />
+            <Link href={'/memos/create'}>
+                <button className="mt-[25px] h-[48px] w-[118px] rounded-[12px] bg-purple-200 text-semibold16 text-[#ffffff]">
+                    기록하러 가기
+                </button>
+            </Link>
         </div>
     );
 }
