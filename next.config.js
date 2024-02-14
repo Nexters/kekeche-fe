dc920b0dd367f413b32d15628be6b7768de39640
Jay Lee <roof2586@naver.com>
2024-02-14 23:57:02 +0900

    .

--- next.config.js
+++ next.config.js
@@ -14,7 +14,6 @@ const withPWA = require('@ducanh2912/next-pwa').default({
 
 const nextConfig = {
     images: {
-        unoptimized: true,
         remotePatterns: [
             {
                 protocol: 'https',
