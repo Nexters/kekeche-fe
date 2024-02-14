a64a71e3c601b45448f5bc5d5f8aeeaf3e37241e
Jay Lee <roof2586@naver.com>
2024-02-15 01:00:55 +0900

    .

--- next.config.js
+++ next.config.js
@@ -2,8 +2,8 @@
 
 const withPWA = require('@ducanh2912/next-pwa').default({
     dest: 'public',
-    cacheOnFrontEndNav: true,
-    aggressiveFrontEndNavCaching: true,
+    cacheOnFrontEndNav: false,
+    aggressiveFrontEndNavCaching: false,
     reloadOnOnline: true,
     disable: false,
     workboxOptions: {
