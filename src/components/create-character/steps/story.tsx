1f43fc6ea3d3798b142541c516abfe7856d253a6
황 태환 <asst2401095@tosspartners.com>
2024-02-19 00:31:22 +0900

    feat: 서비스 온보딩

--- src/components/create-character/steps/story.tsx
+++ src/components/create-character/steps/story.tsx
@@ -29,19 +29,18 @@ export default React.memo(function Story() {
                 }}
                 withText={false}
             />
-
             {storyNum === 1 ? (
                 <>
-                    <Image height={536} priority alt={'1'} src={Story1} className="z-[2] mt-[10px]" />
                     <FixedBottomArea className="mb-[31px] gap-[16px]">
+                        <Image height={400} priority alt={'1'} src={Story1} className="z-[2] mt-[10px]" />
                         <StoryBox text={'내 안의 다양한 공룡들을 키워서, 나의 공룡들과\n함께 다같이 레벨업해요'} />
                         <CtaButton text="다음" onClick={() => setStoryNum(2)} />
                     </FixedBottomArea>
                 </>
             ) : storyNum === 2 ? (
                 <>
-                    <Image height={536} priority alt={'2'} src={Story2} className="z-[2] mt-[10px]" />
                     <FixedBottomArea className="mb-[31px] gap-[16px]">
+                        <Image height={400} priority alt={'2'} src={Story2} className="z-[2] mt-[10px]" />
                         <StoryBox text={'키우고 싶은 능력을 주특기로 설정해요'} />
                         <CtaButton text="다음" onClick={() => setStoryNum(3)} />
                     </FixedBottomArea>
@@ -49,8 +48,8 @@ export default React.memo(function Story() {
             ) : (
                 storyNum === 3 && (
                     <>
-                        <Image width={259} height={536} priority alt={'3'} src={Story3} className="z-[2] mt-[10px]" />
                         <FixedBottomArea className="mb-[31px] gap-[16px]">
+                            <Image height={400} priority alt={'3'} src={Story3} className="z-[2] mt-[10px]" />
                             <StoryBox
                                 text={
                                     '캐릭터에게 그 날의 일기 메모를 먹이로 줄 수 있어요. 메모는 하루에 3개까지 쓸 수 있어요.'
