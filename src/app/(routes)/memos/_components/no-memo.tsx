66c859a50a7fc628c0a886244a50ccc3dd854981
황 태환 <asst2401095@tosspartners.com>
2024-02-20 00:29:14 +0900

    fix: 기록하러 가기 처리

--- src/app/(routes)/memos/_components/no-memo.tsx
+++ src/app/(routes)/memos/_components/no-memo.tsx
@@ -2,14 +2,12 @@
 
 import NoMemoFallback from '@/components/no-memo-fallback';
 import Link from 'next/link';
-import { useRouter } from 'next/navigation';
 
 export default function NoMemo() {
-    const router = useRouter();
     return (
         <div className=" mt-[155px] flex w-full flex-col items-center">
             <NoMemoFallback />
-            <Link href={'/memos/create'}>
+            <Link href={'?write=on'}>
                 <button className="mt-[25px] h-[48px] w-[118px] rounded-[12px] bg-primary-500 text-semibold16 text-[#ffffff]">
                     기록하러 가기
                 </button>
