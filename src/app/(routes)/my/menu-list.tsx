bd636f405a0de1aa1f4d4c97f1c2953512f1c5fb
황 태환 <asst2401095@tosspartners.com>
2024-02-19 23:07:24 +0900

    feat: iOS 앱설치 유도

--- src/app/(routes)/my/menu-list.tsx
+++ src/app/(routes)/my/menu-list.tsx
@@ -30,12 +30,31 @@ export default function MenuList({ member }: Props) {
     const router = useRouter();
     const { toast } = useToast();
     const { deferredPrompt } = useA2HS();
-    const { showInstall: showIosInstall } = useIsIos();
+    const { isIos, showInstall: showIosInstall } = useIsIos();
 
     const showInstall = !!deferredPrompt || showIosInstall;
     const install = () => {
-        //@ts-ignore
-        deferredPrompt?.prompt();
+        if (!isIos) {
+            //@ts-ignore
+            deferredPrompt?.prompt();
+            return;
+        }
+
+        const shareData = {
+            title: '다양한 나를 키우는 AnotherMe',
+            text: '나의 캐릭터별 성장기록 서비스',
+            url: `${location.origin}${ROUTES.characters(member.memberId)}`,
+        };
+
+        if (navigator.canShare && navigator.canShare(shareData)) {
+            navigator.share(shareData);
+        } else {
+            toast({
+                description: 'URL이 클립보드에 복사되었어요.',
+            });
+        }
+        sendGTMEvent({ event: 'clickShare' });
+        return;
     };
 
     return (
