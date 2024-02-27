e760c3fe44a230b6f8fa608e57d7006b7708d7b3
Jay Lee <roof2586@naver.com>
2024-02-28 00:13:59 +0900

    feat: style growth process card

--- src/app/(routes)/member/[memberId]/page.tsx
+++ src/app/(routes)/member/[memberId]/page.tsx
@@ -40,7 +40,7 @@ export default async function Home({ params: { memberId } }: { params: { memberI
                 </header>
                 <section className="mb-5 px-6">
                     <h2 className="mb-[10px] pt-5 text-[20px] font-bold leading-[32px] text-[#494E59]">성장과정</h2>
-                    <div className="flex justify-between gap-[10px] py-[6px]">
+                    <div className="flex justify-between gap-[10px] pb-[6px]">
                         <div className="flex flex-1 items-center justify-between rounded-xl bg-[#F6F8FC] px-5 py-[18px]">
                             <div>
                                 <div className="text-[12px] font-medium leading-[18px] text-[#8B92A0]">성장한지</div>
