8526001fad798e36ac5f9e4249a42e5f2dedf0e0
황 태환 <asst2401095@tosspartners.com>
2024-02-27 20:26:54 +0900

    fix: bottom area 속성 수정

--- src/components/create-character/fixed-bottom-area.tsx
+++ src/components/create-character/fixed-bottom-area.tsx
@@ -7,7 +7,7 @@ type Props = {
 export default function FixedBottomArea({ className, children, style }: Props) {
     return (
         <div
-            className={`fixed bottom-0 flex h-[auto] w-full flex-col items-center pt-[16px] ${className}`}
+            className={`fixed bottom-0 mx-auto flex h-[auto]  w-full min-w-[350px] flex-col items-center items-center pt-[16px] lg:w-[400px] ${className}`}
             style={style}
         >
             {children}
