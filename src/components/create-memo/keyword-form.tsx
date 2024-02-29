9959a74c966c48d865cc55cdbba8e9eb61af2a25
Jay Lee <roof2586@naver.com>
2024-02-29 21:35:31 +0900

    feat: style create character button

--- src/components/create-memo/keyword-form.tsx
+++ src/components/create-memo/keyword-form.tsx
@@ -21,7 +21,7 @@ export default function KeywordForm() {
 
     return (
         <div>
-            <div className="flex gap-1 px-6 py-3">
+            <div className="flex items-center gap-1 px-6 py-3">
                 <svg width="25" height="25" viewBox="0 0 25 25" fill="none" xmlns="http://www.w3.org/2000/svg">
                     <path
                         d="M7.58301 17.3853C14.6541 21.6279 20.3109 15.971 19.6038 5.36449C8.99743 4.65737 3.34082 10.3144 7.58301 17.3853ZM7.58301 17.3853C7.58293 17.3851 7.58309 17.3854 7.58301 17.3853ZM7.58301 17.3853L5.75391 19.2137M7.58301 17.3853L11.4108 13.5569"
