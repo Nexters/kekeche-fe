4be04411781b23028a18857e40b4a46791371082
황 태환 <asst2401095@tosspartners.com>
2024-03-04 23:12:41 +0900

    fix: CTA 버튼 비활성화 시 shadow 제거

--- src/components/ui/cta-button.tsx
+++ src/components/ui/cta-button.tsx
@@ -9,7 +9,7 @@ export default function CTAButton({ children, text, disabled, ...props }: Props)
     return (
         <button
             {...props}
-            className={`cta-button h-[56px] w-[343px] rounded-[16px] text-[18px] font-[600] text-white shadow-[0_0_4px_0_rgba(0,0,0,0.25)] ${disabled ? 'bg-[#7D7D7D]' : ' bg-primary-500'}`}
+            className={`h-[56px] w-[343px] rounded-[16px] text-[18px] font-[600] text-white shadow-[0_0_4px_0_rgba(0,0,0,0.25)]  ${disabled ? 'bg-[#7D7D7D] shadow-none active:shadow-none ' : ' bg-primary-500 active:shadow-[inset_2px_2px_5px_rgba(0,0,0,0.5)]'}`}
         >
             {text}
             {children}
