75d2f7ae261f5aa6313583885cc05f03d246a12b
stakbucks <dhassidu11@gmail.com>
2024-03-11 00:36:06 +0900

    test: 홈 페이지 테스트 코드 개선

--- src/__tests__/home.test.tsx
+++ src/__tests__/home.test.tsx
@@ -2,13 +2,13 @@ import Home from '@/app/page';
 import render from '@/__tests__/setups/render';
 import { screen } from '@testing-library/react';
 import { redirect } from 'next/navigation';
-import { IsLoggedIn, checkIsLoggedIn } from '@/services/auth/getMember';
+import { IsLoggedIn, Member, checkIsLoggedIn } from '@/services/auth/getMember';
 
 vi.mock('@/services/auth/getMember');
 
 describe('홈 페이지 테스트', () => {
     it('로그인 안했을 때 홈 페이지를 렌더링 합니다.', async () => {
-        vi.mocked(checkIsLoggedIn).mockReturnValue(Promise.resolve({ isLoggedIn: false }));
+        vi.mocked(checkIsLoggedIn).mockResolvedValue({ isLoggedIn: false });
         render(await Home());
         const button = screen.getByText('로그인하기');
 
@@ -16,10 +16,9 @@ describe('홈 페이지 테스트', () => {
     });
 
     it('로그인 했을 때는 유저의 홈으로 리다이렉트 합니다.', async () => {
-        const member = { memberId: 2 };
-        vi.mocked(checkIsLoggedIn).mockReturnValue(
-            Promise.resolve({ isLoggedIn: true, member }) as Promise<IsLoggedIn>,
-        );
+        const member: Member = { memberId: 2, characterCount: 2, memoCount: 3 };
+
+        vi.mocked(checkIsLoggedIn).mockResolvedValue({ isLoggedIn: true, member });
 
         vi.mock('next/navigation', () => {
             return {
